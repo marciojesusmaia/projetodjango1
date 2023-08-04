3d32c2a9f20b34467ad12f88352cf54c7ebd0ac5
marciojesusmaia <casodio@hotmail.com>
2023-08-04 07:10:40 -0300

    removido função sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'recipes/home.html')
 
-def sobre(request):
-    return HttpResponse('Pagina sendo criada por Marcio Maia')
+
