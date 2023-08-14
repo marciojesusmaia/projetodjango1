1343f3de49a9e4153fc7e36cf83921c145613221
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-14 13:30:05 -0300

    adicionado modelos teste

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from recipes.utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name':'Marcio Maia'})
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
 
