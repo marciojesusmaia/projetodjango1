495c3529ab42652499a1c4048c40a7044a1f9e1c
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-16 13:24:02 -0300

    adicionado id aleatorios

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from recipes import views
 
-
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
     
 ]   
