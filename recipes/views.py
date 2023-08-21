73c53f83c66574ae4a8efe29b21d139684705252
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-21 12:33:24 -0300

    adicionado informacoes do banco a pagina

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from recipes.utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
+    recipes = Recipe.objects.all().order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(), 'is_detail_page':True,})
