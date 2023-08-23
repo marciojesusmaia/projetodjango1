9157d4af55867eb4c4f48fc6ea68ee9b4546dd0f
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-23 12:45:36 -0300

    adicionando filtro de não publicadas

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from recipes.utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
@@ -11,6 +11,6 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(), 'is_detail_page':True,})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes':recipes,})
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={'recipes':recipes})
 
