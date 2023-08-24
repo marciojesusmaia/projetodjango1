cd2fae66a3e61d8052bb4eb4014c46fdcd4a1615
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-24 12:27:15 -0300

    modificando pages recipes

--- recipes/views.py
+++ recipes/views.py
@@ -12,5 +12,5 @@ def recipe(request, id):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes':recipes})
+    return render(request, 'recipes/pages/category.html', context={'recipes':recipes})
 
