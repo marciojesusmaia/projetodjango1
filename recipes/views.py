87ca0a9712e673bfe1f331f7da377ca1588fb661
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-28 13:30:26 -0300

    edicao na funcao recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.utils.recipes.factory import make_recipe
 from recipes.models import Recipe
-from django.http import HttpResponse
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -12,8 +11,7 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(), 'is_detail_page':True,})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    if not recipes:
-        return HttpResponse('Page Not Found', 404)
-    return render(request, 'recipes/pages/category.html', context={'recipes':recipes, 'title': f'{recipes.first().category.name} - Category |'})
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+
+    return render(request, 'recipes/pages/category.html', context={'recipes':recipes, 'title': f'{recipes[0].category.name} - Category |'})
 
