e92014c0d85290e4117f6d2026571045a2f925c6
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-08-28 13:17:02 -0300

    adicionado page 404 sem categoria

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from recipes.utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import HttpResponse
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -12,5 +13,7 @@ def recipe(request, id):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={'recipes':recipes})
+    if not recipes:
+        return HttpResponse('Page Not Found', 404)
+    return render(request, 'recipes/pages/category.html', context={'recipes':recipes, 'title': f'{recipes.first().category.name} - Category |'})
 
