1eaf434e29ec65cd78484633e6372b1f54da35e0
marciojesusmaia <marciojesusmaia@hotmail.com>
2023-09-04 12:49:27 -0300

    adicionado mais testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 # Create your tests here.
 class RecipeURLsTest(TestCase):
@@ -14,3 +14,16 @@ class RecipeURLsTest(TestCase):
     def test_recipe_home_recipe_test_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id':1})
         self.assertEqual(url, '/recipes/1/')
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertTrue(True)
+
+    def test_recipe_category_views_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id':1,}))
+        self.assertTrue(True)
+
+    def test_recipe_detail_views_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id':1}))
+        self.assertTrue(True)
